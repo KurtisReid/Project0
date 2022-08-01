c246002a4a75659ecaee6d02d21a6042d6257275
Kurtis <kreid8@kent.edu>
2022-08-01 12:01:02 -0400

    Finished interfaces of expenses

--- src/main/java/dev/reid/entity/Expense.java
+++ src/main/java/dev/reid/entity/Expense.java
@@ -4,8 +4,13 @@ public class Expense {
     private int id;
     private double expenseCost;
 
-    public Expense(int id, double expenseCost) {
+    private String status;
+
+    private int employeeIssuer;
+
+    public Expense(int id, double expenseCost, String status) {
         this.id = id;
         this.expenseCost = expenseCost;
+        this.status = status;
     }
 }
